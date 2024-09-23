fcb520bb19d5f1af868f7770649bff5686e8ea8e
DanielErnany <ddernany@gmail.com>
2024-09-23 18:42:26 -0300

    Tela de receitas Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _baseIngredients = false;
+
+  List<String> ingredients = [
+    "farinha de trigo",
+    "açúcar",
+    "chocolate em pó",
+    "fermento em pó",
+    "leite",
+    "ovos",
+    "óleo"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,70 +47,101 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Bolo de Chocolate"),
       ),
-      body: Column(
-        children: [
-          Image.network("https://i.ytimg.com/vi/QFMxJWh3mqE/maxresdefault.jpg"),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 10,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network(
+              "https://i.ytimg.com/vi/QFMxJWh3mqE/maxresdefault.jpg",
             ),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.pink,
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 5,
-                      vertical: 5,
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Icon(Icons.cake, color: Colors.white),
-                        Text(
-                          "Doce",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10,
+              ),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.pink,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 5,
+                        vertical: 5,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(Icons.cake, color: Colors.white),
+                          Text(
+                            "Doce",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const Text(
-                  "Ingredientes",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  const Text(
+                    "Ingredientes",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 5),
-                const Text(
-                  "2 xícaras de farinha de trigo, 1 xícara de açúcar, 1 xícara de chocolate em pó, 1 colher de sopa de fermento em pó, 1 xícara de leite, 3 ovos, 1/2 xícara de óleo",
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Modo de preparo",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text("Base"),
+                      Checkbox(
+                        value: _baseIngredients,
+                        onChanged: (value) {
+                          if (value != null) {
+                            setState(() {
+                              _baseIngredients = value;
+                            });
+                          }
+                        },
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 5),
-                const Text(
-                  "1. Preaqueça o forno a 180°C. 2. Em uma tigela, misture a farinha, o açúcar, o chocolate em pó e o fermento. 3. Adicione os ovos, o leite e o óleo, e misture bem até obter uma massa homogênea. 4. Despeje a massa em uma forma untada e enfarinhada. 5. Leve ao forno por aproximadamente 35 minutos ou até que, ao espetar um palito, ele saia limpo.",
-                ),
-              ],
+                  const SizedBox(height: 5),
+                  _baseIngredients
+                      ? Align(
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: ingredients
+                                .map((element) => Text(element))
+                                .toList(),
+                          ),
+                        )
+                      : const Text(
+                          "2 xícaras de farinha de trigo, 1 xícara de açúcar, 1 xícara de chocolate em pó, 1 colher de sopa de fermento em pó, 1 xícara de leite, 3 ovos, 1/2 xícara de óleo",
+                        ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Modo de preparo",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    "1. Preaqueça o forno a 180°C. 2. Em uma tigela, misture a farinha, o açúcar, o chocolate em pó e o fermento. 3. Adicione os ovos, o leite e o óleo, e misture bem até obter uma massa homogênea. 4. Despeje a massa em uma forma untada e enfarinhada. 5. Leve ao forno por aproximadamente 35 minutos ou até que, ao espetar um palito, ele saia limpo.",
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
